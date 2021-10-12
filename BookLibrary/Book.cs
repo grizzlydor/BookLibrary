48d3b32561291fb657319a469332753ab2bd1d36
grizzlydor <artn2@inbox.lv>
2021-10-12 10:54:41 +0200

    Add project files.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public int ISBN13 { get; set; }
+        
+        public Book()
+        {
+
+        }
+    }
+}
