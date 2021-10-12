a7e0f27f525cdb62c682ef099d501c2e28c126bb
grizzlydor <artn2@inbox.lv>
2021-10-13 00:11:30 +0200

    Added xml comments

--- BookLibrary/Book.cs
+++ BookLibrary/Book.cs
@@ -4,9 +4,21 @@ namespace BookLibrary
 {
     public class Book
     {
+        /// <summary>
+        /// Title of the book
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// Author of the book
+        /// </summary>
         public string Author { get; set; }
+        /// <summary>
+        /// Number of the pages in the book
+        /// </summary>
         public int PageNumber { get; set; }
+        /// <summary>
+        /// An unique identifier of the book
+        /// </summary>
         public int ISBN13 { get; set; }
         
         public Book()
